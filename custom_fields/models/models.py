4c58118c51f13b4ee6f3ba75198b296e877a40b6
Dev <developers@linescripts.com>
2020-05-08 18:56:43 +0530

    [UPDATE]: 1. Added new 23 fields in CRM,SO,INOVICE,PO and VB, 2. Showed booking number field in PO, SO and invoice pdf report. 3. Removed default date from event date field
    	modified:   custom_fields/models/models.py
    	modified:   custom_fields/views/templates.xml
    	modified:   custom_fields/views/views.xml

--- custom_fields/models/models.py
+++ custom_fields/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class CustomSaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    event_date = fields.Datetime(string="Event Date", default=fields.Datetime.now())
+    event_date = fields.Datetime(string="Event Date")
 
     ipcountryname = fields.Text(string="Geoip Country")
     ipcountryiso = fields.Text(string="Geoip Country ISO")
@@ -16,6 +16,38 @@ class CustomSaleOrder(models.Model):
     url1 = fields.Text(string="Url 1")
     source1_date = fields.Datetime(string="Source Date 1")
     gclid = fields.Text(string="Google Click ID")
+
+    # ------------- Booking Fields -----------------
+    bookingnumber = fields.Char(string="Booking number")
+
+    web_formid = fields.Char(string="Form id")
+    web_product = fields.Char(string="Product")
+    web_subproduct = fields.Char(string="Subproduct")
+    web_price = fields.Char(string="Price")
+    web_eventaddress_street = fields.Char(string="Event street")
+    web_eventaddress_street2 = fields.Char(string="Event street 2")
+    web_eventaddress_city = fields.Char(string="Event city")
+    web_eventaddress_zip = fields.Char(string="Event zip")
+    web_eventaddress_country = fields.Char(string="Event country")
+
+    # ------------- Extra information fields -----------------
+
+    web_extra_phone = fields.Char(string="Extra phone")
+    web_participant_firstname = fields.Char(string="Participant first name")
+    web_participant_lastname = fields.Char(string="Participant last name")
+    web_participants_firstnames = fields.Char(string="Birthday first names")
+    web_dateofbirth = fields.Date(string="Date of birth")
+    web_age_year = fields.Char(string="Years")
+    web_age_month = fields.Char(string="Months")
+    web_age_days = fields.Char(string="Days")
+    web_age_year_next = fields.Char(string="NextYears")
+    web_health_issue = fields.Text(string="Health issue")
+    web_health_issue_details = fields.Text(string="Health issue details")
+    web_language = fields.Text(string="Website language")
+
+    # ------------- Message field-----------------
+    web_message = fields.Text(string="Message")
+
 
     @api.multi
     def _prepare_invoice(self):
@@ -34,7 +66,31 @@ class CustomSaleOrder(models.Model):
             'medium1': self.medium1,
             'campaign1':self.campaign1,
             'url1':self.url1,
-            'gclid':self.gclid
+            'gclid':self.gclid,
+            'bookingnumber': self.bookingnumber, 
+            'web_formid': self.web_formid, 
+            'web_product': self.web_product, 
+            'web_subproduct': self.web_subproduct, 
+            'web_price': self.web_price, 
+            'web_eventaddress_street': self.web_eventaddress_street, 
+            'web_eventaddress_street2': self.web_eventaddress_street2, 
+            'web_eventaddress_city': self.web_eventaddress_city, 
+            'web_eventaddress_zip': self.web_eventaddress_zip, 
+            'web_eventaddress_country': self.web_eventaddress_country, 
+            'web_extra_phone': self.web_extra_phone, 
+            'web_participant_firstname': self.web_participant_firstname, 
+            'web_participant_lastname': self.web_participant_lastname, 
+            'web_participants_firstnames': self.web_participants_firstnames, 
+            'web_dateofbirth': self.web_dateofbirth, 
+            'web_age_year': self.web_age_year, 
+            'web_age_month': self.web_age_month, 
+            'web_age_days': self.web_age_days, 
+            'web_age_year_next': self.web_age_year_next, 
+            'web_health_issue': self.web_health_issue, 
+            'web_health_issue_details': self.web_health_issue_details, 
+            'web_language': self.web_language, 
+            'web_message': self.web_message,
+
             })
         return res
 
@@ -59,7 +115,30 @@ class CustomSaleAdvancePaymentInv(models.TransientModel):
             'medium1': order.medium1,
             'campaign1':order.campaign1,
             'url1':order.url1,
-            'gclid':order.gclid
+            'gclid':order.gclid,
+            'bookingnumber': order.bookingnumber, 
+            'web_formid': order.web_formid, 
+            'web_product': order.web_product, 
+            'web_subproduct': order.web_subproduct, 
+            'web_price': order.web_price, 
+            'web_eventaddress_street': order.web_eventaddress_street, 
+            'web_eventaddress_street2': order.web_eventaddress_street2, 
+            'web_eventaddress_city': order.web_eventaddress_city, 
+            'web_eventaddress_zip': order.web_eventaddress_zip, 
+            'web_eventaddress_country': order.web_eventaddress_country, 
+            'web_extra_phone': order.web_extra_phone, 
+            'web_participant_firstname': order.web_participant_firstname, 
+            'web_participant_lastname': order.web_participant_lastname, 
+            'web_participants_firstnames': order.web_participants_firstnames, 
+            'web_dateofbirth': order.web_dateofbirth, 
+            'web_age_year': order.web_age_year, 
+            'web_age_month': order.web_age_month, 
+            'web_age_days': order.web_age_days, 
+            'web_age_year_next': order.web_age_year_next, 
+            'web_health_issue': order.web_health_issue, 
+            'web_health_issue_details': order.web_health_issue_details, 
+            'web_language': order.web_language, 
+            'web_message': order.web_message,
             })
         return invoice
 
@@ -67,7 +146,8 @@ class CustomSaleAdvancePaymentInv(models.TransientModel):
 class CustomPurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    event_date = fields.Datetime(string="Event Date", default=fields.Datetime.now())
+    event_date = fields.Datetime(string="Event Date")
+    bookingnumber = fields.Char(string="Booking number")
 
     @api.multi
     def action_view_invoice(self):
@@ -76,13 +156,14 @@ class CustomPurchaseOrder(models.Model):
         """
         result = super(CustomPurchaseOrder, self).action_view_invoice()
         result['context']['default_event_date'] = self.event_date or fields.Datetime.now()
+        result['context']['default_bookingnumber'] = self.bookingnumber or ''
         return result
 
 
 class CustomAccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    event_date = fields.Datetime(string="Event Date", default=fields.Datetime.now())
+    event_date = fields.Datetime(string="Event Date")
 
     ipcountryname = fields.Text(string="Geoip Country")
     ipcountryiso = fields.Text(string="Geoip Country ISO")
@@ -93,6 +174,37 @@ class CustomAccountInvoice(models.Model):
     url1 = fields.Text(string="Url 1")
     source1_date = fields.Datetime(string="Source Date 1")
     gclid = fields.Text(string="Google Click ID")
+
+    # ------------- Booking Fields -----------------
+    bookingnumber = fields.Char(string="Booking number")
+
+    web_formid = fields.Char(string="Form id")
+    web_product = fields.Char(string="Product")
+    web_subproduct = fields.Char(string="Subproduct")
+    web_price = fields.Char(string="Price")
+    web_eventaddress_street = fields.Char(string="Event street")
+    web_eventaddress_street2 = fields.Char(string="Event street 2")
+    web_eventaddress_city = fields.Char(string="Event city")
+    web_eventaddress_zip = fields.Char(string="Event zip")
+    web_eventaddress_country = fields.Char(string="Event country")
+
+    # ------------- Extra information fields -----------------
+
+    web_extra_phone = fields.Char(string="Extra phone")
+    web_participant_firstname = fields.Char(string="Participant first name")
+    web_participant_lastname = fields.Char(string="Participant last name")
+    web_participants_firstnames = fields.Char(string="Birthday first names")
+    web_dateofbirth = fields.Date(string="Date of birth")
+    web_age_year = fields.Char(string="Years")
+    web_age_month = fields.Char(string="Months")
+    web_age_days = fields.Char(string="Days")
+    web_age_year_next = fields.Char(string="NextYears")
+    web_health_issue = fields.Text(string="Health issue")
+    web_health_issue_details = fields.Text(string="Health issue details")
+    web_language = fields.Text(string="Website language")
+
+    # ------------- Message field-----------------
+    web_message = fields.Text(string="Message")
 
     @api.model
     def create(self,vals):
@@ -112,7 +224,30 @@ class CustomAccountInvoice(models.Model):
                 'medium1': sale_order.medium1,
                 'campaign1':sale_order.campaign1,
                 'url1':sale_order.url1,
-                'gclid':sale_order.gclid
+                'gclid':sale_order.gclid,
+                'bookingnumber': sale_order.bookingnumber, 
+                'web_formid': sale_order.web_formid, 
+                'web_product': sale_order.web_product, 
+                'web_subproduct': sale_order.web_subproduct, 
+                'web_price': sale_order.web_price, 
+                'web_eventaddress_street': sale_order.web_eventaddress_street, 
+                'web_eventaddress_street2': sale_order.web_eventaddress_street2, 
+                'web_eventaddress_city': sale_order.web_eventaddress_city, 
+                'web_eventaddress_zip': sale_order.web_eventaddress_zip, 
+                'web_eventaddress_country': sale_order.web_eventaddress_country, 
+                'web_extra_phone': sale_order.web_extra_phone, 
+                'web_participant_firstname': sale_order.web_participant_firstname, 
+                'web_participant_lastname': sale_order.web_participant_lastname, 
+                'web_participants_firstnames': sale_order.web_participants_firstnames, 
+                'web_dateofbirth': sale_order.web_dateofbirth, 
+                'web_age_year': sale_order.web_age_year, 
+                'web_age_month': sale_order.web_age_month, 
+                'web_age_days': sale_order.web_age_days, 
+                'web_age_year_next': sale_order.web_age_year_next, 
+                'web_health_issue': sale_order.web_health_issue, 
+                'web_health_issue_details': sale_order.web_health_issue_details, 
+                'web_language': sale_order.web_language, 
+                'web_message': sale_order.web_message,
                 }
                 res.update(vals)
 
@@ -122,7 +257,7 @@ class CustomAccountInvoice(models.Model):
 class CustomCRMLead(models.Model):
     _inherit = 'crm.lead'
 
-    event_date = fields.Datetime(string="Event Date", default=fields.Datetime.now())
+    event_date = fields.Datetime(string="Event Date")
 
     ipcountryname = fields.Text(string="Geoip Country")
     ipcountryiso = fields.Text(string="Geoip Country ISO")
@@ -135,6 +270,38 @@ class CustomCRMLead(models.Model):
     gclid = fields.Text(string="Google Click ID")
 
 
+    # ------------- Booking Fields -----------------
+    bookingnumber = fields.Char(string="Booking number")
+
+    web_formid = fields.Char(string="Form id")
+    web_product = fields.Char(string="Product")
+    web_subproduct = fields.Char(string="Subproduct")
+    web_price = fields.Char(string="Price")
+    web_eventaddress_street = fields.Char(string="Event street")
+    web_eventaddress_street2 = fields.Char(string="Event street 2")
+    web_eventaddress_city = fields.Char(string="Event city")
+    web_eventaddress_zip = fields.Char(string="Event zip")
+    web_eventaddress_country = fields.Char(string="Event country")
+
+    # ------------- Extra information fields -----------------
+
+    web_extra_phone = fields.Char(string="Extra phone")
+    web_participant_firstname = fields.Char(string="Participant first name")
+    web_participant_lastname = fields.Char(string="Participant last name")
+    web_participants_firstnames = fields.Char(string="Birthday first names")
+    web_dateofbirth = fields.Date(string="Date of birth")
+    web_age_year = fields.Char(string="Years")
+    web_age_month = fields.Char(string="Months")
+    web_age_days = fields.Char(string="Days")
+    web_age_year_next = fields.Char(string="NextYears")
+    web_health_issue = fields.Text(string="Health issue")
+    web_health_issue_details = fields.Text(string="Health issue details")
+    web_language = fields.Text(string="Website language")
+
+    # ------------- Message field-----------------
+    web_message = fields.Text(string="Message")
+
+
 class CustomProcurementRule(models.Model):
     _inherit = 'procurement.rule'
 
@@ -145,7 +312,8 @@ class CustomProcurementRule(models.Model):
         if result['origin']:
             so = self.env['sale.order'].sudo().search([('name', 'ilike', result['origin'])])
             if so:
-                result.update({'event_date': so.event_date or False})
+                result.update({'event_date': so.event_date or False,
+                                'bookingnumber': so.bookingnumber or False})
         return result
 
     @api.multi
@@ -186,7 +354,8 @@ class CustomProcurementRule(models.Model):
             if not po.event_date:
                 so = self.env['sale.order'].sudo().search([('name', 'ilike', origin)])
                 if so:
-                    po.write({'event_date': so.event_date or False})
+                    po.write({'event_date': so.event_date or False,
+                                'bookingnumber': so.bookingnumber or False})
         # Create Line
         po_line = False
         for line in po.order_line:
